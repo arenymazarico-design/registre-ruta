d1ec059432fea34dfa42fa858a45472dbcc5a6e4
arenymazarico-design <arenymazarico@gmail.com>
2026-07-23 12:28:28 +0200

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -10,7 +10,7 @@
   };
   var CAT_KEYS = Object.keys(CATS);
   var EXPENSE_KEYS = CAT_KEYS.filter(function (k) { return k !== "combustible"; });
-  var APP_VERSION = "2025-07-03 · data-entrada";
+  var APP_VERSION = "2025-07-03 · fila-pintada";
 
   // ---------- Idioma (català per defecte / castellà) ----------
   var lang = localStorage.getItem("lang") || "ca";
